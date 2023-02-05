8961baff55064cd0398f9f2f497b839521285249
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2023-02-05 22:13:54 +0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 export const URL_SERVER = "http://localhost:3000";
-export const SSR_URL_SERVER = "http://localhost:3000";
-// export const SSR_URL_SERVER = "http://unode:3000";
+// export const SSR_URL_SERVER = "http://localhost:3000";
+export const SSR_URL_SERVER = "http://unode:3000";
 
 export const IO_SERVER = "http://localhost:3000";
 
